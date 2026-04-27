c7b16bc2da12d817ef3f9c7c0d59d6c358a1ee28
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2026-04-27 08:47:53 +0200

    Creadas clases bases y carta esqueleto.

---
+++ src/Carta.java
@@ -0,0 +1,22 @@
+public class Carta {
+    private Valor valor;
+    private Palo palo;
+
+    public Carta (Valor valor,Palo palo){
+        this.valor = valor;
+        this.palo = palo;
+    }
+
+    public Palo getPalo() {
+        return palo;
+    }
+
+    public Valor getValor(){
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return valor + " de " + palo;
+    }
+}
---
+++ src/Valor.java
@@ -0,0 +1,4 @@
+public enum Valor {
+    AS, DOS, TRES, CUATRO, CINCO, SEIS,
+    SIETE, OCHO, NUEVE, DIEZ, J, Q, K
+}

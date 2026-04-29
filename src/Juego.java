a5e6510968fc737fb895aaf32bd91d39af743ab1
Slgaot <slobaton940@alumnes.politecnicllevant.cat>
2026-04-29 09:01:42 +0200

    creada clase jugador base, funciona pero mejorable.

--- src/Juego.java
+++ src/Juego.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
+
 public class Juego {
-    public static void main (String [] args){
-        Carta carta = new Carta(Valor.AS, Palo.CORAZONES);
-        System.out.println(carta);
+    public static void main(String[] args) {
 
         Mazo mazo = new Mazo();
+        Jugador j1 = new Jugador("Juan");
+        Jugador j2 = new Jugador("Ana");
 
-        Carta c1 = mazo.robarCarta();
-        Carta c2 = mazo.robarCarta();
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+        jugadores.add(j1);
+        jugadores.add(j2);
 
-        System.out.println(c1);
-        System.out.println(c2);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < jugadores.size(); j++) {
+                jugadores.get(j).recibirCarta(mazo.robarCarta());
+            }
+        }
 
-        System.out.println("Quedan: " + mazo.cartasRestantes());
+        for (int i = 0; i < jugadores.size(); i++) {
+            jugadores.get(i).mostrarMano();
+            System.out.println();
+        }
+
+        System.out.println("Cartas restantes: " + mazo.cartasRestantes());
     }
 }
--- src/Jugador.java
+++ src/Jugador.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Jugador {
+
+    private String nombre;
+    private ArrayList<Carta> mano;
+
+    public Jugador (String nombre) {
+        this.nombre = nombre;
+        this.mano = new ArrayList<>();
+    }
+
+    public void recibirCarta(Carta carta){
+        mano.add(carta);
+    }
+
+    public void mostrarMano() {
+        System.out.println("Mano de " + nombre + ":");
+        for (int i = 0; i < mano.size(); i++) {
+            System.out.println(mano.get(i));
+        }
+    }
 }
